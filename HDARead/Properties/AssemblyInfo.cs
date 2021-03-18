3fd5806438b378c3e2e53d0e60cba43428f6d2e5
Sergio Del Franco <sdelfranco@infoil.com.ar>
2021-03-18 12:03:07 -0300

    * Copyright information updated
    * Set version to 0.3

--- HDARead/Properties/AssemblyInfo.cs
+++ HDARead/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HDARead")]
 [assembly: AssemblyDescription("Simple command line OPC HDA client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alexander Korostelev")]
+[assembly: AssemblyCompany("InfOil")]
 [assembly: AssemblyProduct("HDARead")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Alexander Korostelev, 2021 InfOil")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyVersion("0.3.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
